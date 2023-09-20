fca402115fcaf7219ee7d698936ccba1cb728697
jirayu <jirayu004za@gmail.com>
2023-09-20 17:56:25 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Game Tetris")
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
