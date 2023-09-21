a857181bc48671ef222175c4f2356711616b0a30
jirayu <jirayu004za@gmail.com>
2023-09-22 01:40:38 +0700

    remove method getCellColor because we already have it

--- Grid.py
+++ Grid.py
@@ -13,19 +13,6 @@ class Grid:
             for col in range(self.numCols):
                 print(self.grid[row][col], end = " ")
             print()
-
-    def getCellColor(self):
-
-        darkGray = (26, 31, 40)
-        green = (47, 230, 23)
-        red = (232, 18, 18)
-        orange = (226, 116, 17)
-        yellow = (237, 234, 4)
-        purple = (166, 0, 247)
-        cyan = (21, 204, 209)
-        blue = (13, 64, 216)
-
-        return [darkGray, green, red, orange, yellow, purple, cyan, blue]
     
     def draw(self, screen):
         for row in range(self.numRows):
