a93738dcdc461938518ab2d11071d6d55bb8b5e7
jirayu <jirayu004za@gmail.com>
2023-09-22 02:22:57 +0700

    add class LBlock then init parameter

---
+++ Blocks.py
@@ -0,0 +1,12 @@
+from Block import Block
+from Position import Position
+
+class LBlocks(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
+        }
