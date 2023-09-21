074ea5fa8ce89de78229e74e4ae32384d18d5536
jirayu <jirayu004za@gmail.com>
2023-09-22 01:33:02 +0700

    add class method getCellColors

---
+++ Colors.py
@@ -0,0 +1,17 @@
+class Colors:
+    darkGray = (26, 31, 40)
+    green = (47, 230, 23)
+    red = (232, 18, 18)
+    orange = (226, 116, 17)
+    yellow = (237, 234, 4)
+    purple = (166, 0, 247)
+    cyan = (21, 204, 209)
+    blue = (13, 64, 216)
+
+    @classmethod
+    def getCellColors(cls):
+        return [cls.darkGray, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
+    
+
+    
+    
