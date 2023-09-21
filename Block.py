caa1859074993dbd0f66154556b612632eb6972f
jirayu <jirayu004za@gmail.com>
2023-09-22 01:54:28 +0700

    add import class Colors

--- Block.py
+++ Block.py
@@ -1,6 +1,9 @@
+from Colors import Colors
+
 class Block:
     def __init__(self,id):
         self.id = id
         self.cells = {}
         self.cellSize = 30
         self.rotationState = 0
+        self.colors = Colors.getCellColors()
