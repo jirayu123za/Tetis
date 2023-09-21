786447feb32414ddf998c62b33aca9da8046f34a
jirayu <jirayu004za@gmail.com>
2023-09-22 02:23:39 +0700

    add import pygame, add method draw()

--- Block.py
+++ Block.py
@@ -1,4 +1,5 @@
 from Colors import Colors
+import pygame
 
 class Block:
     def __init__(self,id):
@@ -7,3 +8,10 @@ class Block:
         self.cellSize = 30
         self.rotationState = 0
         self.colors = Colors.getCellColors()
+
+    def draw(self, screen):
+        titles = self.cells[self.rotationState]
+        for title in titles:
+            title_rect = pygame.Rect(title.cols * self.cellSize + 1, title.rows * self.cellSize + 1,
+                                    self.cellSize - 1, self.cellSize - 1)
+            pygame.draw.rect(screen, self.colors[self.id], title_rect)
